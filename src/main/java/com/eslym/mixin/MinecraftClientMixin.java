0d3ee16ee4952cbbc4e6ec5ee66fae5bb0407808
0nepeop1e <engshun.lim@gmail.com>
2022-04-29 11:33:04 +0800

    init

---
+++ src/main/java/com/eslym/mixin/MinecraftClientMixin.java
@@ -0,0 +1,15 @@
+package com.eslym.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(MinecraftClient.class)
+public class MinecraftClientMixin {
+	@Inject(at = @At("HEAD"), method = "shouldFilterText()Z", cancellable = true)
+	private void getTexts(CallbackInfoReturnable<Boolean> info) {
+		info.setReturnValue(false);
+	}
+}
